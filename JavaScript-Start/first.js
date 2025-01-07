0e9a44b0afa9eda5fbfa4623a6f05bc3e972dd62
mraipro <sheikhabdulalem@gmail.com>
2025-01-07 08:04:44 +0600

    Operator-in-JavaScript

--- JavaScript-Start/first.js
+++ JavaScript-Start/first.js
@@ -1,8 +1,25 @@
-const profile = {
- username: "@SheikhAleem",
- isFollow: false,
- followers:123,
- following:787,
+//Arithmatic operators
 
-};
-console.log(typeof profile.isFollow);
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b = ", b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a x b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);
+console.log("a ** b = ", a**b); //a to the power b =25
+
+//Unary operators
+console.log("a = ", a, "& b = ", b);
+
+ console.log("a = ",a);
+
+ console.log("a++ = ",a++);//a+1=5 post incerment
+
+ console.log("++a = ",++a);//1+a=7 pre increment
+ 
+ console.log("a-- = ", a--);//a-1=7 post increment
+
+ console.log("a-- = ", --a);//--a=5 pre increment
