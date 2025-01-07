873fb7cbd6cf1ddf1f64bc58d678facc62ac2c3d
mraipro <sheikhabdulalem@gmail.com>
2025-01-07 10:16:44 +0600

    Lodigla ! not Operaotrs

--- JavaScript-Start/first.js
+++ JavaScript-Start/first.js
@@ -55,9 +55,22 @@
 // console.log( "a<=b", a <= b );
 
 // Logical Operators
+// // let a = 6;
+// // let b = 5;
+// let cond1 = a > b; //true
+// let cond2 = a===5;//true
+// console.log("cond1 && cond2 =", cond1 && cond2);
+// console.log("cond1 && cond2 =", a > b && a===6);
+
+//Logical || Or Operator
+// let a = 6;
+// let b = 5;
+// let cond1 = a > b; //true
+// let cond2 = a===5;
+// console.log("cond1 || cond2 =", cond1 || cond2);
+
+// ! logical Not Operators
 let a = 6;
 let b = 5;
-let cond1 = a > b; //true
-let cond2 = a===5;//true
-console.log("cond1 && cond2 =", cond1 && cond2);
-console.log("cond1 && cond2 =", a > b && a===6);
+
+console.log(" !(6<5) =", !(a===5)) // is ture 
